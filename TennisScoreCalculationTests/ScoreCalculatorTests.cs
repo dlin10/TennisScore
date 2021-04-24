74b48e72f00447813e633c0630e0df6db9ee90ce
Dmitry Linetsky <dmitryl@oz-code.com>
2021-04-24 11:29:21 +0300

    TestPointNames added

--- TennisScoreCalculationTests/ScoreCalculatorTests.cs
+++ TennisScoreCalculationTests/ScoreCalculatorTests.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Xml.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TennisScoreCalculation;
 
 namespace TennisScoreCalculationTests
@@ -26,6 +30,43 @@ namespace TennisScoreCalculationTests
             Assert.AreEqual("0", score.PlayerYScore.Points);
             Assert.AreEqual(0,score.PlayerXScore.Sets.Length);
             Assert.AreEqual(0, score.PlayerYScore.Sets.Length);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(TestPointNames_GetData),DynamicDataSourceType.Method)]
+        public void TestPointNames(int pointsCount, string expected)
+        {
+            WinPoints(true, pointsCount);
+            var score = _scoreCalculator.GetCurrentScore();
+            var actual = score.PlayerXScore.Points;
+            Assert.AreEqual(expected, actual);
+        }
+
+        private static IEnumerable<object[]> TestPointNames_GetData()
+        {
+            yield return new object[] {0, "0"};
+            yield return new object[] { 1, "15" };
+            yield return new object[] { 2, "30" };
+            yield return new object[] { 3, "40" };
+        }
+
+        private void WinPoints(bool playerX, int count)
+        {
+            foreach (var b in Enumerable.Repeat(playerX, count))
+            {
+                _scoreCalculator.Increment(b);
+            }
+        }
+
+        private void WinGameForPlayer(bool playerX)
+        {
+            WinPoints(playerX, 4);
+        }
+
+        private void CreateDeuce()
+        {
+            WinPoints(true, 3);
+            WinPoints(false, 3);
         }
     }
 }
